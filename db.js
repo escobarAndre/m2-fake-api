e32281b230ae072fe76169cf88dc315350ee7a87
André Escobar <andre.escobar@soupilar.com.br>
2022-06-30 23:48:45 -0300

    Remove dj.json

---
+++ db.js
@@ -0,0 +1,89 @@
+module.exports = {
+	products: [
+		{
+			id: 1,
+			title: 'Ilimitado Fixo Brasil',
+			price: 59.5,
+			categorie: 'Fixo',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Internet fixa ilimitada',
+				'Roteador de internet gratuito',
+				'instalação gratuito'
+			]
+		},
+		{
+			id: 2,
+			title: 'Ultimate HD',
+			price: 120.9,
+			categorie: 'TV',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Todos os canais da TV aberta',
+				'Mais de 300 canais da TV fechada',
+				'Canais de esportes exclusivos'
+			]
+		},
+		{
+			id: 3,
+			title: 'Full HD',
+			price: 70.0,
+			categorie: 'TV',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Todos os canais da TV aberta',
+				'Mais de 300 canais da TV fechada'
+			]
+		},
+		{
+			id: 4,
+			title: '100MB',
+			price: 79.9,
+			categorie: 'Internet',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Internet fixa ilimitada',
+				'Roteador de internet gratuito',
+				'instalação gratuito'
+			]
+		},
+		{
+			id: 5,
+			title: '200MB',
+			price: 89.9,
+			categorie: 'Internet',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Internet fixa ilimitada',
+				'Roteador de internet gratuito',
+				'instalação gratuito'
+			]
+		},
+		{
+			id: 6,
+			title: '300MB',
+			price: 100.0,
+			categorie: 'Internet',
+			description: 'Pacote de internet fixa ilimitada',
+			items: [
+				'Internet fixa ilimitada',
+				'Roteador de internet gratuito',
+				'instalação gratuito'
+			]
+		}
+	],
+	categories: [
+		{
+			id: 3,
+			name: 'Internet'
+		},
+		{
+			id: 1,
+			name: 'Fixo'
+		},
+		{
+			id: 2,
+			name: 'TV'
+		}
+	]
+};
